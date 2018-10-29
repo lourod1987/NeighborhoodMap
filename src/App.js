23c8ef42a548e6e6b44c3262abc157e53e449642
lourod1987 <louart87@gmail.com>
2018-10-29 16:32:32 -0500

    adding in input and passing props to input including a call to filter function. Filter function displays only results that include characters within query state. mapping over places returned by foursquare api and creating markers and info windows using that information.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,89 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { load_google_maps, load_yelp_locations } from './Utils';
+import { load_google_maps, load_foursquare_locations } from './Utils';
 
 class App extends Component {
+  state = {
+    query: ''
+  }
+
   componentDidMount() {
     let googleMapsPromise = load_google_maps();
-    let yelpMarkers = load_yelp_locations();
+    let foursquareMarkers = load_foursquare_locations();
 
     Promise.all([
       googleMapsPromise,
-      yelpMarkers
+      foursquareMarkers
     ])
     .then( values => {
       console.log(values);
 
       let google = values[0];
-      
+      let places = values[1].response.groups[0].items;
+      console.log(places);
       this.google = google;
       this.markers = [];
+
       this.infowindow = new google.maps.InfoWindow();
       this.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
+        zoom: 10,
         center: {lat: 29.693112, lng: -95.899897},
         scrollwheel: true
-      })
+      });
+     
+
+      places.map( place => {
+        let marker = new google.maps.Marker({
+          position: { lat: place.venue.location.lat, lng: place.venue.location.lng },
+          map: this.map,
+          // place: place,
+          id: place.venue.id,
+          name: place.venue.name,
+          animation: google.maps.Animation.DROP
+        });
+
+        marker.addListener('click', () => {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+          setTimeout( () => { marker.setAnimation(null) }, 1500);
+        });
+
+        google.maps.event.addListener(marker, 'click', () => {
+          this.infowindow.setContent(marker.name);
+          this.map.setZoom(13);
+          this.map.setCenter(marker.position);
+          this.infowindow.open(this.map, marker);
+          this.map.panBy(0, -125);;
+        });
+
+        this.markers.push(marker);
+      });
+
+      
+    });
+  }
+
+  filter = query => {
+    this.markers.filter( marker => {
+      marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
+        marker.setVisible(true) :
+        marker.setVisible(false);
     })
+
+    this.setState({ query });
   }
 
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
       <div>
-        <div id="map" className="App"></div>
-        <div id="sidebar" className="App"></div>
+        <div id="map"></div>
+        <div id="sidebar" className="App">
+          <input value={this.state.query} onChange={ evt => {this.filter(evt.target.value)} } />
+        </div>
       </div>
     );
   }
