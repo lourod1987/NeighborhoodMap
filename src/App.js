bf3ad7a72aa2fea2d8bb143559d26352ec808d55
lourod1987 <louart87@gmail.com>
2018-10-31 16:59:41 -0500

    importing MapStyle.js for use in creating google map. Adding state for filteredPlaces and tracking whether filter menu is hidden with filterMenu. Add styles call to map creation. Add marker color and shape changes. Code to filter list in sidebar added to filter function. Hide menu function added. List selected function added which on list item click select the appropriate marker. Restructured JSX code to better align with accessibility standards and UI requirements

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { load_google_maps, load_foursquare_locations } from './Utils';
+import { styles } from './MapStyle';
 
 class App extends Component {
   state = {
     query: '',
-    locations: []
+    locations: [],
+    filteredPlaces: [],
+    filterMenu: true
   }
 
   componentDidMount() {
@@ -18,29 +21,43 @@ class App extends Component {
       foursquareMarkers
     ])
     .then( values => {
-      // console.log(values);
+      console.log(values);
 
       let google = values[0];
       let places = values[1].response.groups[0].items;
-      console.log(places);
+      // console.log(places);
       this.google = google;
       this.markers = [];
+      const defaultMarker = makeMarkerIcon('0091ff');
+      const selectedMarker = makeMarkerIcon('FFFF24');
 
       this.infowindow = new google.maps.InfoWindow();
       this.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
         center: {lat: 29.693112, lng: -95.899897},
-        scrollwheel: true
+        scrollwheel: true,
+        styles: styles
       });
-     
+
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21, 34));
+        return markerImage;
+      }
 
       places.map( place => {
         let marker = new google.maps.Marker({
           position: { lat: place.venue.location.lat, lng: place.venue.location.lng },
           map: this.map,
           // place: place,
+          icon: defaultMarker,
           id: place.venue.id,
           name: place.venue.name,
+          addr: place.venue.location.formattedAddress,
           animation: google.maps.Animation.DROP
         });
 
@@ -54,11 +71,12 @@ class App extends Component {
           } else {
             marker.setAnimation(google.maps.Animation.BOUNCE);
           }
-          setTimeout( () => { marker.setAnimation(null) }, 1500);
+          // google.maps.Marker.setIcon(selectedMarker);
+          setTimeout( () => { marker.setAnimation(null) }, 2000);
         });
 
         google.maps.event.addListener(marker, 'click', () => {
-          this.infowindow.setContent(marker.name);
+          this.infowindow.setContent(`${marker.name} <br> ${marker.addr[0]} ${marker.addr[1]}`);
           this.map.setZoom(14);
           this.map.setCenter(marker.position);
           this.infowindow.open(this.map, marker);
@@ -66,39 +84,85 @@ class App extends Component {
         });
 
         this.markers.push(marker);
+
+        // marker.addListener('click', () => {
+        //   this.setIcon(selectedMarker);
+        // });
+        return '';
       });
 
+      this.setState({
+        filteredPlaces: this.state.locations
+      });
       
     });
   }
 
   filter = query => {
+    let filteredLocations = this.state.locations.filter( location => {
+      return location.toLowerCase().includes(query.toLowerCase());
+    });
+
     this.markers.filter( marker => {
-      marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
+      return marker.name.toLowerCase().includes(query.toLowerCase()) === true ?
         marker.setVisible(true) :
         marker.setVisible(false);
-    })
+    });
 
-    this.setState({ query });
+    this.setState({
+      filteredPlaces: filteredLocations,
+      query
+    });
+    return '';
+  }
+
+  hideMenu = () => {
+    return this.state.filterMenu === true ?
+    this.setState({ filterMenu: false }) :
+    this.setState({ filterMenu: true });
+  }
+
+  listSelect = selected => {
+    let marker = this.markers.filter( m => m.name === selected)[0];
+    console.log(marker);
+    this.infowindow.setContent(`${marker.name} <br>${marker.addr[0]} ${marker.addr[1]}`);
+    // this.map.setZoom(14);
+    this.map.setCenter(marker.position);
+    this.infowindow.open(this.map, marker);
+    this.map.panBy(0, -125);
+
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(this.google.maps.Animation.BOUNCE);
+    }
+    setTimeout( () => { marker.setAnimation(null) }, 2000);
   }
 
   render() {
-    const { locations, query } = this.state;
+    const { filteredPlaces, query, filterMenu } = this.state;
     return (
       <div>
-      {console.log("locations: " + locations)}
-        <header><h1>Local Entertainment</h1></header>
-        <div id="map"></div>
-        <div id="sidebar" className="App">
-          <input value={query} onChange={ evt => {this.filter(evt.target.value)} } placeholder="Search" className="search"/>
-          {locations.length > 0 && (
-            <ul>
-              {locations.map( location => (
-                <li key={location} className="locations-list">{location}</li>
-              ))}
-            </ul>
-          )}
-        </div>
+        <header>
+          <button className="menu-button" onClick={() => this.hideMenu()}>Menu</button>
+          <h1>Local Entertainment</h1>
+        </header>
+        <main>
+          <div aria-label="Map" role="application" id="map"></div>
+          { filterMenu === false ? '' :
+            <aside id="sidebar" className="App">
+              <input value={query} onChange={ evt => {this.filter(evt.target.value)} } placeholder="Filter Locations" className="search"/>
+              {filteredPlaces.length > 0 && (
+                <ul>
+                  {filteredPlaces.map( location => (
+                    <li key={location} className="locations-list" onClick={ () => {this.listSelect(location)} }>{location}</li>
+                  ))}
+                </ul>
+              )}
+              <p>Locations provided by Foursquare</p>
+            </aside>
+          }
+        </main>
       </div>
     );
   }
