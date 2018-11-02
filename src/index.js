5a52f3d5b7a7a4126cbece493bcb165e0f4309fe
lourod1987 <louart87@gmail.com>
2018-11-02 16:02:00 -0500

    removing commented out code

--- src/index.js
+++ src/index.js
@@ -10,26 +10,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// const cachedFiles = [
-//         '/',
-//         '../public/index.html',
-//         '/index.js',
-//         '/App.css',
-//         '/App.js',
-//         '/Components/Header.js',
-//         '/Components/Map.js',
-//         '/Components/Sidebar.js',
-//         '/Utitlities/MapStyle.js',
-//         '/Utitlities/Utils.js',
-//         '/menu.svg'
-//     ];
 serviceWorker.register();
-
-
-// if ('serviceWorker' in navigator) {
-//         navigator.serviceWorker
-//             .register('/sw.js')
-//             .catch( error => {
-//                 console.log(error);
-//             });
-//     }
