b5859b55f189d2b14ae3511bb834beae93728e40
lourod1987 <louart87@gmail.com>
2018-11-02 03:52:13 -0500

    uncommenting menu.svg credit and giving it a class so that I can hide it.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,7 +12,12 @@ export class Header extends Component {
     return (
         <header>
           <button aria-label="Menu" className="menu-button" onClick={() => toggleMenu()}>
-          {/* <div>Icons made by <a href="https://www.flaticon.com/authors/cole-bemis" title="Cole Bemis">Cole Bemis</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
+          <div className="menuImg-credit">
+            Icons made by 
+            <a href="https://www.flaticon.com/authors/cole-bemis" title="Cole Bemis">Cole Bemis</a> 
+            from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> 
+            is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+          </div>
             <img src={menu} className="menu-icon" alt=""></img>
           </button>
           <h1>Local Entertainment</h1>
