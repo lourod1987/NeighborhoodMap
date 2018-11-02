6ac96866a18eca93e31a5825cdb10ca80f5cdf95
lourod1987 <louart87@gmail.com>
2018-11-02 00:19:32 -0500

    changing starter className

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Map (props) {
     return (
-        <div aria-label="Map" role="application" id="map" className="map-sidebar"></div>
+        <div aria-label="Map" role="application" id="map" className="map"></div>
     );
 }
 
